ec55cc1f48b530b02be22172c72431a82341cb38
Lucas Wong <kar.hang@hotmail.com>
2022-10-13 00:16:29 +0800

    Using Orbit Controls

--- src/script.js
+++ src/script.js
@@ -1,20 +1,8 @@
 import './style.css'
 import * as T from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-/*
-    Cursor
-*/
-const cursor = {
-    x: 0,
-    y: 0
-}
-window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX / sizes.width - 0.5
-    cursor.y = - (event.clientY / sizes.height - 0.5)
-
-})
-
-//SCene
+//Scene
 const scene = new T.Scene();
 
 //Object
@@ -49,9 +37,17 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+const canvas = document.querySelector("canvas.webgl")
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 2;
+// controls.update();
+
 //Renderer
 const renderer = new T.WebGLRenderer({
-    canvas: document.querySelector("canvas.webgl")
+    canvas: canvas
 })
 
 renderer.setSize(sizes.width, sizes.height)
@@ -64,11 +60,8 @@ const tick = () => {
     
     const elapsedTime = clock.getElapsedTime()
 
-    //Update camera
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 5
-    camera.lookAt(mesh.position)
+    //Update controls for every frame to enable damping effect
+    controls.update();
 
     //Render
     renderer.render(scene, camera)
